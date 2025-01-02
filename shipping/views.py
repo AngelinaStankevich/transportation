0085106d731814200787f8c81fea3d13fec4a159
Angelina <angst2003@gmail.com>
2025-01-02 22:11:46 +0300

    new templates

--- shipping/views.py
+++ shipping/views.py
@@ -3,19 +3,19 @@ from django.http import HttpResponse
 from .models import Vehicle, Service, Order
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'shipping/home.html')
 
 def vehicles_list(request):
     vehicles = Vehicle.objects.all()
-    return render(request, 'vehicles_list.html', {'vehicles': vehicles})
+    return render(request, 'shipping/vehicles_list.html', {'vehicles': vehicles})
 
 def services_list(request):
     services = Service.objects.all()
-    return render(request, 'services_list.html', {'services': services})
+    return render(request, 'shipping/services_list.html', {'services': services})
 
 def driver_schedule(request):
     if request.user.is_authenticated and hasattr(request.user, 'driver'):
         orders = Order.objects.filter(driver=request.user.driver)
-        return render(request, 'driver_schedule.html', {'orders': orders})
+        return render(request, 'shipping/driver_schedule.html', {'orders': orders})
     else:
         return HttpResponse("Unauthorized", status=401)
--- transportation/urls.py
+++ transportation/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shipping/', include('shipping.urls'))
+    path('', include('shipping.urls'))
 ]
