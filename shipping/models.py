e72c73d6f0c41fce0aea8b374e9ed61b92e66944
Angelina <angst2003@gmail.com>
2025-01-03 22:44:51 +0300

    drivers filter

--- shipping/models.py
+++ shipping/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class VehicleType(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
@@ -8,12 +9,14 @@ class VehicleType(models.Model):
     def __str__(self):
         return self.name
 
+
 class BodyType(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
 
 class Vehicle(models.Model):
     name = models.CharField(max_length=100)
@@ -25,6 +28,7 @@ class Vehicle(models.Model):
     def __str__(self):
         return f"{self.name} ({self.license_plate})"
 
+
 class Driver(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100)
@@ -34,12 +38,14 @@ class Driver(models.Model):
     def __str__(self):
         return self.full_name
 
+
 class Client(models.Model):
     name = models.CharField(max_length=100)
     contact_info = models.TextField()
 
     def __str__(self):
         return self.name
+
 
 class Organization(models.Model):
     name = models.CharField(max_length=100)
@@ -49,6 +55,7 @@ class Organization(models.Model):
     def __str__(self):
         return self.name
 
+
 class Service(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
@@ -57,12 +64,14 @@ class Service(models.Model):
     def __str__(self):
         return self.name
 
+
 class CargoType(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
 
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
@@ -73,6 +82,8 @@ class Order(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     date = models.DateTimeField()
     cost = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=100, choices=[('pending', 'Pending'), ('completed', 'Completed')],
+                              default='pending')
 
     def __str__(self):
         return f"Order {self.id} for {self.client.name}"
